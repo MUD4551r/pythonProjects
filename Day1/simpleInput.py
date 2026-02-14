f91fd50e86fc18f26e0123e6c813c38092395bde
MUD4551r <mudas7928@gmail.com>
2026-02-14 16:42:43 +0500

    first day: Day1

---
+++ Day1/simpleInput.py
@@ -0,0 +1,55 @@
+# name = input("What is Your name : ")
+# age = input("What is Your age : ")
+
+# print(f"hi {name} your age is {age}")
+
+# num1 = int(input("Enter number : "))
+# num2 = int(input("Enter number :"))
+
+# op = input("Enter operation (+, -, *, /) : ")
+
+# if op == "+":
+#     print(num1+num2)
+# elif op == "-":
+#     print(num1 - num2)
+# elif op  == "*":
+#     print(num1*num2)
+# elif op == "/":
+#     print(num1/num2)
+# else:
+#     print("Oooooops")
+    
+# var1 = 4
+# var2 = 3
+
+# var1 = var1 + var2
+# var2 = var1 - var2
+# var1 = var1 - var2
+
+# print(var1, var2)
+
+# # =========
+# celcius = float(input("Enter C "))
+
+# farenheit = (celcius * 1.8) + 32
+
+# print(f"{celcius} C is {farenheit} F")
+# # ===============
+
+# num = int(input("Enter number : "))
+
+# if num % 2 == 0:
+#     print(f"{num} is even")
+# else:
+#     print(f"{num} is odd")
+# # ========================
+
+h = float(input("Enter height cm : "))
+w = float(input("Enter width cm:"))
+
+area = h * w
+
+print(f"total area of rectangle is {area} cm")
+# =========================
+for i in range(5):
+    print("muda")
